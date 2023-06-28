23cfd4781037fbdb20f1021686df34bee909531c
natashacorreaa <107068670+natashacorreaa@users.noreply.github.com>
2023-06-28 18:29:35 +0530

    hi

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -55,66 +55,8 @@ const TeamSection = ({ teamName, teamMembers }) => {
 							</div>
 						)
 					)
-						return (
-							domain !== ' vice captain' && (
-								<div
-									className='grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 mt-20'
-									key={domain_id}
-								>
-									<div  style={{ fontFamily: 'Audiowide'}}>
-										<h3 className='text-3xl font-light ml-6'>{domain}</h3>
-									</div>
-									<div className='grid grid-cols-4 min-[320px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-4 col-span-2  sm:col-span-2 md:col-span-2 min-[320px]:mt-10'>
-										{teamMembers[domain].map((member, member_id) => {
-											// for each domain in team
-											return (
-												<div key={member_id}>
-													<TeamMember
-														name={member.name}
-														imagePath={member.imagePath}
-														designation={member.designation}
-														specialStyle={member.specialStyle}
-														specialStyle2={member.specialStyle2}
-													/>
-	
-												</div>
-											);
-											
-										})}
-									</div>
-								</div>
-							)
-					);
-					return (
-						domain !== 'm anager' && (
-							<div
-								className='grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 mt-20'
-								key={domain_id}
-							>
-								<div  style={{ fontFamily: 'Audiowide'}}>
-									<h3 className='text-3xl font-light ml-6'>{domain}</h3>
-								</div>
-								<div className='grid grid-cols-4 min-[320px]:grid-cols-2 sm:grid-cols-2 md:grid-cols-4 col-span-2  sm:col-span-2 md:col-span-2 min-[320px]:mt-10'>
-									{teamMembers[domain].map((member, member_id) => {
-										// for each domain in team
-										return (
-											<div key={member_id}>
-												<TeamMember
-													name={member.name}
-													imagePath={member.imagePath}
-													designation={member.designation}
-													specialStyle={member.specialStyle}
-													specialStyle2={member.specialStyle2}
-												/>
-
-											</div>
-										);
-										
-									})}
-								</div>
-							</div>
-						)
-				);
+						
+					
 				})
 			}
 		</section>
