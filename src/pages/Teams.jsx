65f8f4ae095b922ecd25d9cb640146348229a9f3
natashacorreaa <107068670+natashacorreaa@users.noreply.github.com>
2023-06-28 18:36:11 +0530

    small

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -5,6 +5,7 @@ const Teams = () => {
 	return (
 		<div className='container mx-auto px-10 mb-20'>
 			<h1 className='text-6xl font-medium text-center py-10'  style={{ fontFamily: 'Audiowide'}}>Teams</h1>
+			<TeamSection teamName={'Beetle 5.0'} teamMembers={Beetle4} />
 			<TeamSection teamName={'Beetle 4.0'} teamMembers={Beetle4} />
 			<TeamSection teamName={'Beetle 3.0'} teamMembers={Beetle3} />
 			<TeamSection teamName={'Beetle 2.0'} teamMembers={Beetle2} />
